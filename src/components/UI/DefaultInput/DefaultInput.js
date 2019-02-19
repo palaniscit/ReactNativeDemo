54921ae3f5a717eedf50485b45139bec46c71476
Pal <palaniscit@gmail.com>
2019-02-19 11:31:10 +0530

    auth action and toggle login/signup added.

--- src/components/UI/DefaultInput/DefaultInput.js
+++ src/components/UI/DefaultInput/DefaultInput.js
@@ -5,7 +5,7 @@ const defaultInput = props => (
     <TextInput 
         underlineColorAndroid="transparent" 
         {...props}
-        style={[styles.input, props.style]} />
+        style={[styles.input, props.style, !props.valid && props.touched ? styles.invalid : null]} />
 );
 
 const styles = StyleSheet.create({
@@ -16,6 +16,10 @@ const styles = StyleSheet.create({
         padding: 5,
         marginTop: 8,
         marginBottom: 8
+    },
+    invalid: {
+        backgroundColor: "#f9c0c0",
+        borderColor: "red"
     }
 });
 
